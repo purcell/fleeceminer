8c643c66d8543786b02f6c81387a164f732391e3
Steve Purcell <steve.purcell@powershop.co.nz>
2015-07-13 14:59:36 +1200

    ALL THE OPTIMISATIONS

--- fleeceminer.rb
+++ fleeceminer.rb
@@ -10,8 +10,8 @@ module Fleeceminer
   ALLOWED_SOLUTION_CHARS = ('a'..'z').to_a + ('0'..'9').to_a
   OWNER_KEY = "steve"
   SOLUTION_PREFIX = "f1eece"
-  NUM_WORKERS = 5
-  CHECK_INTERVAL = 0.2 # seconds
+  NUM_WORKERS = 4
+  CHECK_INTERVAL = 2.9 # seconds
 
   Request = Struct.new(:start, :attempt)
   Response = Struct.new(:start, :solution)
@@ -22,20 +22,18 @@ module Fleeceminer
       @latest = latest
     end
 
-    def try_solution(attempt)
-      if solution = solve(@latest, attempt)
-        @output.puts(solution)
-        exit
+    def run(start, incr)
+      cur = start
+      prefix = "#{@latest},#{OWNER_KEY},"
+      while true
+        solution = prefix + cur.to_s
+        if (digest = Digest::SHA2.hexdigest(solution)).start_with?(SOLUTION_PREFIX)
+          @output.puts([solution, digest].join('|'))
+          exit
+        end
+        cur += incr
       end
     end
-
-    def solve(start, attempt)
-      new_hash = [start, OWNER_KEY, attempt].join(",")
-      if Digest::SHA2.hexdigest(new_hash).start_with?(SOLUTION_PREFIX)
-        new_hash
-      end
-    end
-
   end
 
   class Supervisor
@@ -48,24 +46,20 @@ module Fleeceminer
     end
 
     def run
+      just_found_one = false
       while true
-        task_workers_with(fetch_latest)
-        wait_for_solution
+        task_workers_with(fetch_latest) unless just_found_one
+        just_found_one = wait_for_solution
       end
     end
 
     def start_workers(latest)
+      STDERR.puts("Tasking workers with #{latest}")
       (0...NUM_WORKERS).each do |n|
         rd, wr = IO.pipe
         @worker_streams << rd
         @worker_pids << fork do
-          STDERR.puts("Starting worker #{n}")
-          worker = Worker.new(wr, latest)
-          cur = 'a'
-          while true
-            worker.try_solution(cur) if (cur.bytes.first % NUM_WORKERS) == n
-            cur = cur.succ
-          end
+          Worker.new(wr, latest).run(n, NUM_WORKERS)
         end
       end
     end
@@ -73,40 +67,41 @@ module Fleeceminer
     def wait_for_solution
       if ready = IO.select(@worker_streams, [], [], CHECK_INTERVAL)
         ready.first.each do |worker_output|
-          solution = worker_output.readline.strip
+          solution, digest = worker_output.readline.strip.split("|")
           if solution.start_with?(@latest)
-            STDERR.write("SOLUTION! #{solution}")
-            task_workers_with(solution)
+            STDERR.puts("SOLUTION! #{solution}")
+            task_workers_with(digest)
             response = Net::HTTP.post_form(URI('https://fleececoin.herokuapp.com/coins'), "coin" => solution)
             STDERR.puts("Response #{response.code}:\n#{response.body}")
-            return
+            if response.code == 400 && response.body =~ /latest hash \((f1eece.*?)\)/
+              # Quickly recover without doing another fetch
+              task_workers_with($1)
+            end
+            return response.code == 200
           else
-            STDERR.write("Warning: Stale solution received")
+            STDERR.puts("Warning: Stale solution received")
           end
         end
       end
+      false
     end
 
     def task_workers_with(latest)
-      if @latest != latest
-        @latest = latest
-        STDERR.puts("New task: #{@latest}")
-        @worker_pids.each { |p| Process.kill("HUP", p) }
-        @worker_pids = []
-        @worker_streams.each(&:close)
-        @worker_streams = []
-        start_workers(latest)
-      else
-        STDERR.puts("Solution unchanged")
-      end
+      return if @latest == latest
+      @latest = latest
+      @worker_pids.each { |p| Process.kill("HUP", p) }
+      @worker_pids = []
+      @worker_streams.each(&:close)
+      @worker_streams = []
+      start_workers(latest)
     end
 
     def fetch_latest
+      STDERR.puts("Fetching latest")
       open('https://fleececoin.herokuapp.com/current', &:read)
     end
 
   end
 end
-
 
 Fleeceminer::Supervisor.new.run if $0 == __FILE__
